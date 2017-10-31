4a5372ef70a23b147a36e7a1fe9dc5de7291bbbf
Max Stockner <mail@bluemaex.de>
2017-10-31 23:02:35 +0100

    version bump and fix configs

---
+++ api/test/Util/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Cyberschall.
+ *
+ * (c) Max Stockner <mail@bluemaex.de>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Bluemaex\Test\Util;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
